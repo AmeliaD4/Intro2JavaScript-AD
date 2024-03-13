6a2e5ee1bfe436f1b1a6869c8cbcef30486d1129
A.D <ameliad15@nycstudents.net>
2024-03-13 15:51:25 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+  function myfuncti0n(){
+    document.getElementById("gr33t").style.color="darkgray";
+    //Makes the button click change the color of the heading.*//
+}
+
+ function zefunction() {
+   document.body.style.backgroundColor="#6f8c7d";
+ }
+
+function rainbow() {
+  document.getElementById("diez").style.color="white";
+}
+
